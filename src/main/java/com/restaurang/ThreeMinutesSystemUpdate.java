42e7681a98b5c62aae42e3e33ce30c0fcbc75eef
Bjorang <anders.bjorang@iths.se>
2025-12-02 12:54:55 +0100

    Updaterat Pom fil java 21.Samt Testat medGeminiKod

--- src/main/java/com/restaurang/ThreeMinutesSystemUpdate.java
+++ src/main/java/com/restaurang/ThreeMinutesSystemUpdate.java
@@ -3,14 +3,16 @@ import java.time.*;
 
 public class ThreeMinutesSystemUpdate {
 
+
+    
 private final Clock clock;
 
    
-    public ThreeMinutesSystemUpdate(Clock clock){
+     public ThreeMinutesSystemUpdate(Clock clock){
         this.clock = clock; 
     }
      public int getThreeMinutesCounter(){
-        return (int)(clock.millis()/180000);
+        return (int)(clock.millis() / 180000);
 
     }
 }
--- src/test/java/com/restaurang/ThreeMinutesSystemUpdateTest.java
+++ src/test/java/com/restaurang/ThreeMinutesSystemUpdateTest.java
@@ -1,18 +1,23 @@
 package com.restaurang;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import java.time.*;
-
+import java.time.Clock;
+import static org.mockito.Mockito.*;
 import org.junit.jupiter.api.Test;
 
 public class ThreeMinutesSystemUpdateTest {
 
-    ThreeMinutesSystemUpdate tmsu = new ThreeMinutesSystemUpdate(Clock);
     @Test 
     
     void ThreeMinutesUpdate(){
-    
+    Clock clockMock = mock(Clock.class);
+
+
+     when(clockMock.millis()).thenReturn(540000L);  
+
+
+    ThreeMinutesSystemUpdate tmsu = new ThreeMinutesSystemUpdate(clockMock);
+
     int threeMinutesPassed = tmsu.getThreeMinutesCounter();
     assertEquals(3, threeMinutesPassed);
     }
