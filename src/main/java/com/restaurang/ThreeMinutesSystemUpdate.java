4265224241ee6cf6cab147c2dcca1275fc8ea0e7
Bjorang <anders.bjorang@iths.se>
2025-12-02 13:28:40 +0100

    Added Comment to code.

--- src/main/java/com/restaurang/ThreeMinutesSystemUpdate.java
+++ src/main/java/com/restaurang/ThreeMinutesSystemUpdate.java
@@ -3,16 +3,16 @@ import java.time.*;
 
 public class ThreeMinutesSystemUpdate {
 
-
+// Koden är AI genererad för att lära oss. 
     
 private final Clock clock;
 
    
-     public ThreeMinutesSystemUpdate(Clock clock){
+     public ThreeMinutesSystemUpdate(Clock clock){  //En okänd klock som vi kan använda för fejka tiden med hjälp av Mockito
         this.clock = clock; 
     }
      public int getThreeMinutesCounter(){
         return (int)(clock.millis() / 180000);
-
+        //return 3;
     }
 }
--- src/test/java/com/restaurang/ThreeMinutesSystemUpdateTest.java
+++ src/test/java/com/restaurang/ThreeMinutesSystemUpdateTest.java
@@ -8,15 +8,15 @@ import org.junit.jupiter.api.Test;
 public class ThreeMinutesSystemUpdateTest {
 
     @Test 
-    
+    // Koden är AI skriven för att få ett fungerande exmpel där vad som händer. 
     void ThreeMinutesUpdate(){
-    Clock clockMock = mock(Clock.class);
+    Clock clockMock = mock(Clock.class); //våran StuntDubbel
 
 
-     when(clockMock.millis()).thenReturn(540000L);  
+     when(clockMock.millis()).thenReturn(540000L);   //Value 540000L är våran Stubbe 
 
 
-    ThreeMinutesSystemUpdate tmsu = new ThreeMinutesSystemUpdate(clockMock);
+    ThreeMinutesSystemUpdate tmsu = new ThreeMinutesSystemUpdate(clockMock); //Vi hämtar värdet från main.
 
     int threeMinutesPassed = tmsu.getThreeMinutesCounter();
     assertEquals(3, threeMinutesPassed);
