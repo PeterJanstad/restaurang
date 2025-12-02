9eff12af3cd01d35f08aa9ebec288093252d276d
Bjorang <anders.bjorang@iths.se>
2025-12-02 09:04:36 +0100

    Removed AppTest

---
+++ src/test/java/com/restaurang/MaxAntalPlatserTest.java
@@ -0,0 +1,13 @@
+package com.restaurang;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class MaxAntalPlatserTest {
+    
+    @Test
+    void maxSeatShouldBeSixty(){
+        assertEquals(60,60);
+    }
+}
